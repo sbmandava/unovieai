57d529b7b71a698524a51183a0dfc3f575eb1ab5
vdesibabu <vdesibabu@vdesibabus-MacBook-Pro.local>
2025-01-25 15:00:23 -0600

    new update v2 site

--- repo/sandbox-llm.sh
+++ repo/sandbox-llm.sh
@@ -111,9 +111,9 @@ mkdir -p /opt/projects
 cd /tmp
 wget --no-check-certificate https://unovie.ai/repo/sales-kg.tgz
 cd /opt/projects
-tar -xvzf /tmp/sales-kg.tgz .
-cd /opt/projects/sales-kg/templates
-cp * /root/.config/io.datasette.llm/templates
+tar -xvzf /tmp/sales-kg.tgz 
+# cd /opt/projects/sales-kg/templates
+# cp * /root/.config/io.datasette.llm/templates
 }
 
 
